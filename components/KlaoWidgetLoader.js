a96804edb3eb63dfcf142ce2304cee8430c2a55c
Mohamed Ayoub <mohamedosamaayoup202676@gmail.com>
2026-07-15 18:15:18 +0300

    test staging

--- components/KlaoWidgetLoader.js
+++ components/KlaoWidgetLoader.js
@@ -5,10 +5,10 @@ import Script from "next/script";
 export default function KlaoWidgetLoader() {
   return (
     <Script
-      src="https://cdn.klao.eu/widget/development/latest/klao-widget.js"
+      src="https://cdn.klao.eu/widget/staging/latest/klao-widget.js"
       onLoad={() => {
         window.KLAOWidget.init({
-          widgetToken: "wgt_a7169a23aed83d3c8303b0754fb9d2b0",
+          widgetToken: "wgt_880932875edd4fd5695d306f18160e42",
            color: '#11652f',        // accent color (any CSS color)
            position: 'bottom-right', // 'bottom-left' | 'top-right' | 'top-left'
         });
